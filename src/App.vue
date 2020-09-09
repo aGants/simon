a69a0f4f69b6038ca80b0d5257c9508243d3e5a1
Adelia <adelgants@gmail.com>
2020-09-10 02:06:41 +0300

    add style

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,62 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+* {
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif
+}
+
+.game {
+  width: 310px;
+  height: 320px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  &-block {
+    width: 150px;
+    height: 150px;
+  }
+}
+
+.btn {
+  width: 310px;
+  height: 30px;
+  margin: 20px auto 25px;
+  text-align: center;
+  cursor: pointer;
+  background: #E7ECFF;
+  outline: none;
+  border: none;
+  &:hover {
+    border: 2px solid #C6D8FF;
+  }
+  &:active {
+    background: #C6D8FF;
+  }
+}
+
+.radio {
+  margin: 25px 8px;
+  cursor: pointer;
+}
+
+.label {
+  cursor: pointer;
+}
+
+.red { background: #FED6BC; }
+.blue { background: #DEF7FE; }
+.green { background: #C3FBD8; }
+.yellow { background: #FFFADD; }
+
 </style>
