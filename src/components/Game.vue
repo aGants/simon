a116926671624d0d3fdfdd7ec0f1c6c2b8060928
Adelia <adelgants@gmail.com>
2020-09-10 02:12:50 +0300

    add game

---
+++ src/components/Game.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    <div class="game">
+      <div class="game-block red"></div>
+      <div class="game-block blue"></div>
+      <div class="game-block green"></div>
+      <div class="game-block yellow"></div>
+    </div>
+    <div class="setting">
+      <button class="btn">Старт</button>
+      <p>Раунд: <b>{{ round }}</b></p>
+      <label class="label">
+        <input type="radio" name='lvl' value='easy' checked class='radio'> Лёгкий
+      </label>
+      <label class="label">
+        <input type="radio" name='lvl' value='medium' class='radio'> Средний
+      </label>
+      <label class="label">
+        <input type="radio" name='lvl' value='hard' class='radio'> Сложный
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  data() {
+    return {
+      round: 0,
+    }
+  }
+}
+</script>
